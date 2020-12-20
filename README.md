79627258baab1d948eed5f1120e057c0dc5274ca
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2020-12-20 18:37:45 +0100

    Solo codigo bro

--- README.md
+++
@@ -1,3 +0,0 @@
-# TIS
-  Este repositorio se usará para albergar la webpage usada para el TIS de la asignatura de Ética y legislación profesional de la Universidad Complutense de Madrid.
-  https://astronautasucm.github.io/TIS/.
--- about-us.php
+++ about-us.php
@@ -1,14 +1,16 @@
 <html>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
     <link rel="icon" href="img/icon.png" type="image/png">
     <title>TIS - Inicio</title>
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" type="text/css" href="css/w3.css"/>
-    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="css/w3.css"/>   
     <!-- Bootstrap files (jQuery first, then Popper.js, then Bootstrap JS) -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
 
 
 <body>
@@ -21,30 +23,27 @@
         </button>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav">
-              <li class="nav-item active"> <a class="nav-link" href="index.html">Inicio </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="index.html">Inicio </a> </li>
               <li class="nav-item"><a class="nav-link" href="documentation.html"> Documentacion </a></li>
-              <li class="nav-item"><a class="nav-link" href="game.html"> Juego </a></li>	
+              <li class="nav-item"><a class="nav-link" href="game.html"> Juego </a></li>
+              <li class="nav-item active"><a class="nav-link" href="about-us.html"> Sobre nosotros </a></li>	
         </div> <!-- navbar-collapse.// -->
       </div><!-- container //  -->
       </nav>
     <!--                                    End Header area                                                -->
+    <!--                                    Body area                                                      -->
+    <div class="grad1">
 
-<div class="grad1">
-    <div class="content">
-      <div name="introduccion">
-        <h2>¿Que pretendemos?</h2>
-        <div class="section1"></div>
-      </div>
-    </div>
-    
-  </div>  
-
+    </div>    
+    <!--                                    End body area                                                  -->
     <!--                                    Footer area                                                    -->
     <footer class="page-footer font-small black pt-4 footer text-center">
         Proyecto desarrollado por el grupo de los astronautas 
-            para la asignatura ELP de la <a href="informatica.ucm.es">Facultad de informática de la UCM </a></a>
+            para la asignatura ELP de la <a href="https://informatica.ucm.es/">Facultad de informática de la UCM </a></a>
         
     </footer>    
     <!--                                    End footer area                                                -->
 </body>
 </html>
+
+<!--                                        Scripts                                                        -->
