937fb83f9b23023c3c39e639bcf641057d467d3d
Pablo Vazquez <pavazq01@ucm.es>
2020-12-20 17:56:24 +0100

    add

--- django/TIS/web/models.py
+++ django/TIS/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Case(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=2000)
+    release_date = models.DateField()
