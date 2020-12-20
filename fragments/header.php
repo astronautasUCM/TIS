57cd856314aace7e70a874ae67dfc49e3df0cbdb
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2020-12-20 18:56:39 +0100

    Fixes en los headers

--- fragments/header.php
+++ fragments/header.php
@@ -6,10 +6,10 @@
         </button>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav">
-              <li class="nav-item"> <a class="nav-link" href="index.html">Inicio </a> </li>
-              <li class="nav-item active"><a class="nav-link" href="documentation.html"> Documentacion </a></li>
-              <li class="nav-item"><a class="nav-link" href="game.html"> Juego </a></li>	
-              <li class="nav-item"><a class="nav-link" href="about-us.html"> Sobre nosotros </a></li>	
+              <li class="nav-item"> <a class="nav-link" href="index.php">Inicio </a> </li>
+              <li class="nav-item active"><a class="nav-link" href="documentation.php"> Documentacion </a></li>
+              <li class="nav-item"><a class="nav-link" href="game.php"> Juego </a></li>	
+              <li class="nav-item"><a class="nav-link" href="about-us.php"> Sobre nosotros </a></li>	
         </div> <!-- navbar-collapse.// -->
       </div><!-- container //  -->
 </nav>
--- index.php
+++ index.php
@@ -15,23 +15,7 @@
 
 
 <body>
-    <!--                                    Header area                                                    -->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-primary header">
-        <div class="container">
-        <a class="navbar-brand" href="index.html">ELP - TIS</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="main_nav">
-          <ul class="navbar-nav">
-              <li class="nav-item active"> <a class="nav-link" href="index.html">Inicio </a> </li>
-              <li class="nav-item"><a class="nav-link" href="documentation.html"> Documentacion </a></li>
-              <li class="nav-item"><a class="nav-link" href="game.html"> Juego </a></li>
-              <li class="nav-item"><a class="nav-link" href="about-us.html"> Sobre nosotros </a></li>	
-        </div> <!-- navbar-collapse.// -->
-      </div><!-- container //  -->
-      </nav>
-    <!--                                    End Header area                                                -->
+    <!--                                    Header area                                                    -->             
     <?php
         require 'fragments/header.php';
     ?> 
