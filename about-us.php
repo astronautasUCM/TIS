dec54be500b42b1bde8d010a2090ce2af06d8e9e
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2020-12-21 14:02:31 +0100

    Fixes

--- about-us.php
+++ about-us.php
@@ -11,21 +11,23 @@
     ?> 
     <!--                                    End Header area                                                -->
     <!--                                    Body area                                                      -->
-    <div class='container grad1'>
-        <div class='mb-5'></div>
-        <?php
-            $persons = array(
-                'pab' => array('Pablo', 'Jefe de Equipo', 'Esta a tope'),
-                'ped' => array('Pedro', 'Pesao profesional', 'Maquinote con movidas web'));
-            $r = true;
-            foreach ($persons as $person) {
-                $r = $r xor true;
-                $name = $person[0];
-                $title = $person[1];
-                $description = $person[2];
-                require 'fragments/person.php';
-            }
-        ?>
+    <div class="grad1">
+        <div class='container'>
+            <div class='mb-5'></div>
+            <?php
+                $persons = array(
+                    'pab' => array('Pablo', 'Jefe de Equipo', 'Esta a tope'),
+                    'ped' => array('Pedro', 'Pesao profesional', 'Maquinote con movidas web'));
+                $r = true;
+                foreach ($persons as $person) {
+                    $r = $r xor true;
+                    $name = $person[0];
+                    $title = $person[1];
+                    $description = $person[2];
+                    require 'fragments/person.php';
+                }
+            ?>
+        </div>
     </div>
     <!--                                    End body area                                                  -->
     <!--                                    Footer area                                                    -->
