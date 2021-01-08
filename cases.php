3aff63e8f4583b739937d3a56169d5d383bdb5ab
Pablo Vazquez <pavazq01@ucm.es>
2021-01-08 14:28:35 +0100

    targetas

--- cases.php
+++ cases.php
@@ -22,7 +22,30 @@
         <div name="title">
             <h2>Casos más importantes</h2>
         </div>
-      
+        <div class="container">
+            <div class="row align-items-center">
+                <?php
+                    $cases = array(
+                        'case1' => array('Caso 1', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'link1'),
+                        'case2' => array('Caso 2', 'asdfasdfadsf', 'ESP4567466/342', 'Juzgado de Madrid', 'link2'),
+                        'case3' => array('Caso 3', 'Hola hola!!!!', 'ESP456/3434534532', 'Juzgado de Pais Basco', 'link3'),
+                        'case4' => array('Caso 4', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'link4'),
+                        'case5' => array('Caso 5', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'link5')
+                    );
+                    foreach ($cases as $case) {
+                        $title = $case[0];
+                        $description = $case[1];
+                        $casenumber = $case[2];
+                        $location = $case[3];
+                        $link = $case[4];
+                        echo '<div class="col-sm">';
+                        require 'fragments/card-case.php';
+                        echo "</div>";
+                    }
+                ?>
+            </div>
+            </div>
+        
     </div>    
     <!--                                    End body area                                                  -->
     <!--                                    Footer area                                                    -->
---
+++ fragments/card-case.php
@@ -0,0 +1,10 @@
+
+
+<div class="card" style="width: 18rem; color: black">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $title ?></h5>
+    <p class="card-text"><?php echo $description ?></p>
+    <a href="<?php echo $link ?>" class="btn btn-primary"><?php echo $casenumber ?></a>
+  </div>
+</div>
