cd1c3b16284aefbb7e3d5089e23d79f2f745b8a7
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-08 14:01:48 +0100

    Añadido carousel cases

--- cases.php
+++ cases.php
@@ -1,7 +1,9 @@
 <html>
+<link rel="stylesheet" type="text/css" href="css/cases.css"/>
     <?php
         require 'fragments/imports.php';
     ?> 
+     
     <title>TIS - Casos</title>
 <body>
     <!--                                    Header area                                                    -->
@@ -11,8 +13,15 @@
     <!--                                    End Header area                                                -->
     
     <!--                                    Body area                                                      -->
-    <div class="grad1">
-
+    <div class="grad1 web-body">
+        <div name="title">
+            <h2>Casos más importantes</h2>
+        </div>
+        <div class="cases-carousel">
+            <?php
+                require 'fragments/casesCarousel.php';
+            ?> 
+        </div>
     </div>    
     <!--                                    End body area                                                  -->
     <!--                                    Footer area                                                    -->
