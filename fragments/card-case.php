c4e28456c5d35afc543c641c8514e0a0aa6897a0
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-14 13:00:28 +0100

    More fixes

--- fragments/card-case.php
+++ fragments/card-case.php
@@ -2,10 +2,11 @@
 
 
 <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-    <title><?php echo $title ?></title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+    <a href="<?php echo $link ?>" >
+        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="<?php echo $image ?>" preserveAspectRatio="xMidYMid slice" focusable="false">
+    </a>
     <div class="card-body">
+        <h4 style="color:black"><?php echo $title ?></h4>
         <p class="card-text" style="color:black;"><?php echo $description ?></p>
         <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
