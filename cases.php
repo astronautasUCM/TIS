f65fb9301121850af78f19a2840a1922c6bdc597
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-14 12:49:26 +0100

    Añadida la URL para las imágenes

--- cases.php
+++ cases.php
@@ -27,11 +27,11 @@
         <div class="row row-cols-1 row-cols-sm-3 row-cols-md-3 g-3">
                 <?php
                     $cases = array(
-                        'case1' => array('Caso 1', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia1.php'),
-                        'case2' => array('Caso 2', 'asdfasdfadsf', 'ESP4567466/342', 'Juzgado de Madrid', 'pages/noticias/noticia2.php'),
-                        'case3' => array('Caso 3', 'Hola hola!!!!', 'ESP456/3434534532', 'Juzgado de Pais Basco', 'pages/noticias/noticia3.php'),
-                        'case4' => array('Caso 4', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia4.php'),
-                        'case5' => array('Caso 5', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia5.php')
+                        'case1' => array('Caso 1', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia1.php', 'img/news/1.jpg'),
+                        'case2' => array('Caso 2', 'asdfasdfadsf', 'ESP4567466/342', 'Juzgado de Madrid', 'pages/noticias/noticia2.php', 'img/news/2.jpg'),
+                        'case3' => array('Caso 3', 'Hola hola!!!!', 'ESP456/3434534532', 'Juzgado de Pais Basco', 'pages/noticias/noticia3.php', 'img/news/3.jpg'),
+                        'case4' => array('Caso 4', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia4.php', 'img/news/4.jpg'),
+                        'case5' => array('Caso 5', 'El caso mas importante de la carrera politica de barcenas', 'ESP456/342', 'Juzgado de Barcelona', 'pages/noticias/noticia5.php', 'img/news/5.jpg')
                     );
                     foreach ($cases as $case) {
                         $title = $case[0];
@@ -39,6 +39,7 @@
                         $casenumber = $case[2];
                         $location = $case[3];
                         $link = $case[4];
+                        $image = $case[5];
                         echo '<div class="col p-2">';
                         require 'fragments/card-case.php';
                         echo "</div>";
