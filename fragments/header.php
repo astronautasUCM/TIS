89cffc24741c973bb999a47408bd8497cc872377
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-15 00:15:01 +0100

    prueba htaccess

--- fragments/header.php
+++ fragments/header.php
@@ -5,16 +5,16 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top header">
         <div class="container container-x">
-        <a class="navbar-brand title" href="index.php">ELP - TIS</a>
+        <a class="navbar-brand title" href="./">ELP - TIS</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/judgements.php") {echo 'active';}?>" href="judgements.php">Sentencias</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/cases.php") {echo 'active';}?>" href="cases.php">Noticias</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/documentation.php") {echo 'active';}?>" href="documentation.php">Como reportar</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/about-us.php") {echo 'active';}?>" href="about-us.php">Sobre nosotros</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/judgement") {echo 'active';}?>" href="judgements">Sentencias</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/cases") {echo 'active';}?>" href="cases">Noticias</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/documentation") {echo 'active';}?>" href="documentation">Como reportar</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "/TIS/about-us") {echo 'active';}?>" href="about-us">Sobre nosotros</a></li>
         </div> <!-- navbar-collapse.// -->
       </div><!-- container //  -->
 </nav>
