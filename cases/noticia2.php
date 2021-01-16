62e2fdee5c6291c92c250bc270008b12f7091249
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-16 18:38:20 +0100

    Añadidas noticias

--- cases/noticia2.php
+++ cases/noticia2.php
@@ -3,12 +3,25 @@
 
 <?php 
     $titulo = "Cuatro meses de prisión y multa a dos alumnos por ‘hackear’ ordenadores de profesores";
-    $description = "Un juzgado de la localidad pontevedresa de A Estrada condenó a cuatro meses de cárcel y multa de 480 euros a dos alumnos de un instituto por hackear ordenadores de varios profesores. Instalaron un software que daba acceso a las contraseñas de sus correos electrónicos para conocer las preguntas de los exámenes que eran divulgadas por un grupo de WhatsApp.
-    Ambos acusados llegaron a un acuerdo con la Fiscalía tras confesar los hechos, que derivó en una sentencia de conformidad por los delitos de interceptación ilegal de datos informáticos y revelación de secretos. Ninguno cumplirá la condena al ser la pena inferior a dos años y no tener antecedentes penales.
+    $titulo = "Una sentencia pionera absuelve a una persona por compartir cine en la Red";
+    $description1 = 'Un juzgado condenó a dos estudiantes por hackear ordenadores de profesores con una multa de 480 y cuatro meses de cárcel. Los alumnos habían 
+    instalado un software que daba acceso a contraseñas para conocer las preguntas de los exámenes y estos las enviaban a sus compañeros por grupos de whatsapp para 
+    que todos conociesen las preguntas. Los dos imputados quedaron absueltos al ser la pena menor a dos años, llegaron a un acuerdo con la fiscalía al confesar los 
+    hechos y esto derivó una sentencia de conformidad.
+    ';
+
+    $description2 = 'El hackeo fue denunciado por la dirección del IES Manuel García Barros en marzo de 2017 al descubrirse que había ciertas anomalías en el sistema 
+    como que en los exámenes los estudiantes copiaban al dedillo las respuestas de los exámenes o que los sistemas informáticos funcionaban de una manera extraña. 
+    Fue el primer caso de este estilo que sucedió en Galicia. Aunque el único uso era obtener las preguntas del examen también se vio comprometida información personal 
+    que estaban en los ordenadores de los profesores. La Xunta de Galicia tuvo que revisar los sistemas informáticos de los centros educativos viendo que no habían sido 
+    suficientes y “burlados” muy fácilmente.
+    ';
+
+    $description3 = 'Detuvieron a 5 alumnos e implicaron a 8 que fueron relacionados con este caso que afectaba a las cuentas privadas de 27 profesores. Un alumno declaró 
+    que él no instaló el software pero sí conocía como usarlo y que el solo uso esto para revisar un par de notas pero no obtuvo datos privados ni ningún tipo de examen.
+    Otro de los alumnos afirmó lo mismo que el anterior pero este sí entró en cuentas de profesores aunque no divulgó ningún tipo de información recogida en sus investigaciones.';
+
     
-    Uno de los alumnos investigados reconoció que “no instaló el programa pero sí conocía que el mismo se encontraba instalado, que efectuó únicamente dos entradas para comprobar que funcionaba y para ver las notas de una asignatura, pero no accedió a datos de la vida privada de ningún profesor ni divulgó ningún dato de los obtenidos”, según recoge la investigación judicial.
-    El otro alumno también implicado afirmó que “conocía la instalación del sistema software pero que no participó en su instalación, pese a que accedió a varias cuentas de profesores por curiosidad, pero que no divulgó datos de carácter personal”.
-    ";
     require 'fragments/imports.php';
     require 'fragments/header.php';
    
--- cases/noticia3.php
+++ cases/noticia3.php
@@ -3,12 +3,25 @@
 
 <?php 
     $titulo = "Una sentencia pionera absuelve a una persona por compartir cine en la Red";
-    $description1 = "Un juzgado de la localidad pontevedresa de A Estrada condenó a cuatro meses de cárcel y multa de 480 euros a dos alumnos de un instituto por hackear ordenadores de varios profesores. Instalaron un software que daba acceso a las contraseñas de sus correos electrónicos para conocer las preguntas de los exámenes que eran divulgadas por un grupo de WhatsApp.
-    Ambos acusados llegaron a un acuerdo con la Fiscalía tras confesar los hechos, que derivó en una sentencia de conformidad por los delitos de interceptación ilegal de datos informáticos y revelación de secretos. Ninguno cumplirá la condena al ser la pena inferior a dos años y no tener antecedentes penales.
-    
-    Uno de los alumnos investigados reconoció que “no instaló el programa pero sí conocía que el mismo se encontraba instalado, que efectuó únicamente dos entradas para comprobar que funcionaba y para ver las notas de una asignatura, pero no accedió a datos de la vida privada de ningún profesor ni divulgó ningún dato de los obtenidos”, según recoge la investigación judicial.
-    El otro alumno también implicado afirmó que “conocía la instalación del sistema software pero que no participó en su instalación, pese a que accedió a varias cuentas de profesores por curiosidad, pero que no divulgó datos de carácter personal”.
-    ";
+    $description1 = 'La sentencia en ningún momento cuestionó la ilegalidad de la descarga de contenidos digitales (en este caso un película), determinó 
+    que no era posible culpar al titular de la línea por difundir contenido a través de ella. El Juzgado nº 1 de San Sebastián dictaminó que el acusado por 
+    difundir una película a través de la red iba a resultar absuelto debido a la escasez de pruebas para poder culparlo y que sufriera una condena. 
+    Esta decisión tuvo mucha repercusión al ser la primera sentencia absolutoria relacionada con este tema.   
+    ';
+
+    $description2 = 'El Juez Pedro José Malagón Ruiz determinó que no era suficiente culpar de esta infracción sólo por la dirección IP. Esto le sirvió al 
+    denunciante para librarse así de la condena por haber difundido la película en la red para el uso de otros internautas. Esta decisión dio lugar a que 
+    otras sentencias iguales que estaban en el aire por toda la península sobre casos similares, fueran resueltas y los acusados absueltos. Aunque no se sentará 
+    jurisprudencia hasta que en algún momento se pronuncie el Tribunal Supremo. El caso, conocido con el nombre de "Dallas Buyer Club" 
+    (es el título de la película compartida),  siguió abierto en varios juzgados y cientos de denunciantes aprovecharon esta absolución para poner sus sentencias a su 
+    favor y así salir ellos también impunes de la sentencia.
+    ';
+
+    $description3 = 'Un despacho de abogados en Bilbao envió cartas a 300 personas que residían en Bilbao, multandolos con 475 euros por haber “pirateado” la película 
+    “Dallas Buyer Club” esta infracción quería que quedase en algo “amistoso” para no tener que imponer multas mayores. Además si no abonaban está cantidad serían 
+    demandados al no haber querido solucionar el altercado por la vía amistosa que se había establecido en un primer momento. Hubo muchos casos similares en países 
+    como Australia y Alemania, solventandose en este último con cartas avisándoles de lo que estaban haciendo y llegando a multar si no paraban de hacerlo.
+    ';
     require 'fragments/imports.php';
     require 'fragments/header.php';
    
