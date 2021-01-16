7391b0a7ed8574faaf029410cf7b972c455fb2cd
Pablo Vazquez <pavazq01@ucm.es>
2021-01-16 18:40:48 +0100

    About us

--- about-us.php
+++ about-us.php
@@ -25,11 +25,11 @@
         <h2 class="featurette-heading text-center mb-5">El Equipo</h2>
             <?php
                 $persons = array(
-                    'pab' => array('Pablo', 'Jefe de Equipo', 'Esta a tope'),
-                    'ped' => array('Pedro', 'Pesao profesional', 'Maquinote con movidas web'),
-                    'fre' => array('Freeman', 'Twittero en su tiempo ocupado', 'K pasa, hacemos la etica o k pasa'),
-                    'lui' => array('Luis', 'Luisito Comunica', 'Dibuja de vez en cuando paginas web'),
-                    'fra' => array('Fran', 'Futbolero Instagramer', 'GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL del bicho, vaya maquina el madrid como pilotaba el bicho')
+                    'fre' => array('Alex Díaz', 'Redaccion', 'Redactor e investigador de la sección de Sentencias'),
+                    'lui' => array('Luis', 'Redaccion', 'Redactor e investigador de la sección de Noticias'),
+                    'fra' => array('Fran', 'Redaccion', 'Redactor e investigador de la sección de Noticias'),
+                    'ped' => array('Pedro Martínez', 'Desarrollo Web', 'Encargado del desarrollo de la pagina web'),
+                    'pab' => array('Pablo Vázquez', 'Desarrollo Web', 'Encargado del desarrollo de la pagina web')
                 );
                 $r = true;
                 foreach ($persons as $person) {
