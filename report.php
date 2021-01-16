8ed715916e13336d741233656de8d82237239e06
Pablo Vazquez <pavazq01@ucm.es>
2021-01-16 23:49:45 +0100

    Formulario

--- report.php
+++ report.php
@@ -14,6 +14,7 @@
     <!--                                    Body area                                                      -->
     <div class="grad1">
         <h1>¿Cómo reporto una brecha de seguridad?</h1>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScEjCzqoDL-0YKCSZ_0cZpxybaOjRxsUVQu_1sFiebGRvE5Zw/viewform?embedded=true" width="640" height="1737" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
     </div>    
     <!--                                    End body area                                                  -->
     <!--                                    Footer area                                                    -->
