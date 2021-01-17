b2553cac57fbfd722f6afedfb3fed64cea41eb21
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-17 19:38:21 +0100

    Como reportar acabado

--- report.php
+++ report.php
@@ -31,14 +31,40 @@
 
             <div class="row top">
                 <div class="left">
-                    <img class="rnd" src="img/report/1.jpg"  height="100%"/>
+                    <img class="rnd" src="img/report/2.jpg"  height="90%"/>
                 </div>
                 <div class="right text">
-                    <p>En la actualidad tanto personas como empresas están en el punto de mira de poder sufrir un ataque cibernético y 
-                    que esto conlleve la pérdida de datos personales. Por eso es importante reportar estos ataques a tiempo, para interceptar si es 
-                    un malware, un fallo de software, fraude…</p>
-                    <p>INCIBE-CERT tiene un equipo que está activo las 24 horas del día durante los 7 días de la semana, de esta forma resulta muy sencillo 
-                    poder denunciar cualquier delito telemático a las unidades técnicas de las Fuerzas y Cuerpos de Seguridad del Estado.</p>
+                    <p>También hay disponible un correo electrónico <b>(incidencias@incibe-cert.es)</b> y un número de teléfono <b>(017)</b> en el cual se puede reportar cualquier incidente informático 
+                        que hayas sufrido, o rellenando un formulario que proporciona INCIBE-CERT, es necesario detallar bien lo que ha sucedido y cuantos más datos 
+                        proporcionemos más fácil será la resolución del incidente, si la incidencia contiene información privada o sensible recomiendan cifrarla con PGP.
+                    </p>
+                    <a style=" text-align: center; color: white; text-decoration: underline;" href="https://www.incibe.es/protege-tu-empresa/reporte-fraude">Enlace con información para reportar al incibe</a>
+                </div>
+            </div>
+
+            <div class="row top">
+                <div class="left text">
+                    <p>Tras esto, deberíamos notificar a <b>la policía o guardia civil</b> en juzgados de guardia o comisarías. También es posible denunciarlo a la <b>Ertzaintza, la Policía Foral o a los Mossos d´Esquadra</b>.
+                        Hay que facilitar toda la información posible como ya sucedía con INCIBE-CERT, esta información puede ser aportada en formato digital o en papel. 
+                        Se pondrá la correspondiente denuncia e incluso el personal 
+                        especializado en ataques cibernéticos de la policía podrá solicitarnos información adicional que pueda resultar útil en las diligencias.</p>
+                    <p>Una vez presentada la denuncia ésta será tratada tanto por la Guardia Civil a través del Grupo de Delitos Telemáticos, como por el Cuerpo Nacional 
+                        de Policía a través de la Brigada de Investigación Tecnológica.</p>
+                </div>
+                <div class="right">
+                    <img class="rnd" src="img/report/3.jpg"  height="100%"/>
+                </div>
+            </div>
+
+            <div class="row top bottom">
+                <div class="left">
+                    <img class="rnd" src="img/report/4.jpg"  height="100%"/>
+                </div>
+                <div class="right text">
+                    <p>Además de estas dos opciones hay empresas privadas las cuales ayudan ante ataques cibernéticos e inclusive a su prevención con software especializado 
+                        para poder evitarlos. Estas empresas ofrecen un servicio similar al de INCIBE-CERT con correo de contacto y teléfono de atención al cliente para la 
+                        resolución del problema.
+                    </p>
                 </div>
             </div>
 
