90d3d6cf678837050a2793bcee0907cbe612aabe
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-17 21:59:04 +0100

    Update header.php

--- fragments/header.php
+++ fragments/header.php
@@ -11,11 +11,11 @@
         </button>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link <?php if($uri == "http://ismyweblegal.tk/judgements.php") {echo 'active';}?>" href="judgements.php">Sentencias</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "http://ismyweblegal.tk/cases.php") {echo 'active';}?>" href="cases.php">Noticias</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "http://ismyweblegal.tk/report.php") {echo 'active';}?>" href="report.php">Cómo reportar</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "http://ismyweblegal.tk/test.php") {echo 'active';}?>" href="test.php">Test</a></li>
-            <li class="nav-item"><a class="nav-link <?php if($uri == "http://ismyweblegal.tk/about-us.php") {echo 'active';}?>" href="about-us.php">Sobre nosotros</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "ismyweblegal.tk/judgements.php") {echo 'active';}?>" href="judgements.php">Sentencias</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "ismyweblegal.tk/cases.php") {echo 'active';}?>" href="cases.php">Noticias</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "ismyweblegal.tk/report.php") {echo 'active';}?>" href="report.php">Cómo reportar</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "ismyweblegal.tk/test.php") {echo 'active';}?>" href="test.php">Test</a></li>
+            <li class="nav-item"><a class="nav-link <?php if($uri == "ismyweblegal.tk/about-us.php") {echo 'active';}?>" href="about-us.php">Sobre nosotros</a></li>
         </div> <!-- navbar-collapse.// -->
       </div><!-- container //  -->
 </nav>
