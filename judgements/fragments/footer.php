5234dda26b6e1163a5f1ebde02a2f91fc532cb71
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-17 19:55:44 +0100

    Añadidos enlaces a la licencia

--- judgements/fragments/footer.php
+++ judgements/fragments/footer.php
@@ -1,5 +1,5 @@
 <footer class="page-footer font-small black pt-4 footer text-center">
         Proyecto desarrollado por el grupo de los astronautas 
             para la asignatura ELP de la <a href="https://informatica.ucm.es/">Facultad de informática de la UCM </a></a>
-        <p style="text-align:center"><img src="../img/license.png"></p>
+            <p style="text-align:center"><a href="https://creativecommons.org/licenses/?lang=es"><img src="../img/license.png"></a></p>
 </footer>   
