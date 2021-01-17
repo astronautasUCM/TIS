646889100e7deb815f24d8e07c2bab185f578ab0
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-01-17 21:43:39 +0100

    Update about-us.php

--- about-us.php
+++ about-us.php
@@ -36,7 +36,9 @@
             </p>
             <p>Esta página web es una recopilación de datos e información de diferentes fuentes encontradas en internet, desde noticias publicadas en periódicos hasta sentencias 
             judiciales. Pretende ser un centro de información donde encontrar una diversidad de contenido redactado de forma objetiva en un solo lugar, y poder leer y comprender los 
-            posibles delitos que han cometido otras personas para que con suerte no te ocurra lo mismo.
+            posibles delitos que han cometido otras personas para que con suerte no te ocurra lo mismo. Todas las imágenes de esta web han sido obtenidas de pixabay, un banco de imágenes
+            libres de derechos de autor. Todos los contenidos se publican bajo la Licencia Pixabay, que los hace seguros para usar sin pedir permiso o dar crédito al artista, incluso con fines comerciales.
+            <a href="https://pixabay.com/es/">enlace</a>.
             </p>
             <p>También se encuentra en esta página un link a una encuesta (estimación de 2 minutos en completar) si deseas compartir de forma anónima cuáles eran tus conocimientos 
             sobre los delitos informáticos que ocurren en internet antes de leer el contenido de esta página.</p>
